c78bf7a99fbac1dd7e59686688db342b3e363fcc
MuhamadAhmadin <ahmadinations@gmail.com>
2023-04-20 19:04:37 +0700

    setup router

---
+++ src/views/transaction/Create.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>Create Page</h3>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/transaction/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>Index Page</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
